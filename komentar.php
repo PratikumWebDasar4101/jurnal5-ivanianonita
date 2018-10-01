5a0beef2cb2a2e2f0adb3f9047dedcddaee7509d
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-10-01 10:18:48 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,42 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table>
+		<form action="validasi.php" method="POST">
+			<tr>
+				<td>Nim</td>
+				<td>:</td>
+				<td><input type="number" maxlength="10" name="nim"></td>	
+			</tr>
+
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td><input type="text" maxlength="25" name="nama"></td>
+			</tr>
+
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td><input type="email" name="email"></td>
+			</tr>
+
+			<tr>
+				<td>Komentar</td>
+				<td>:</td>
+				<td><input type="text" name="komentar"></td>
+			</tr>		
+
+			<tr>
+				<td><input type="submit" name="submit" value="submit"></td>
+			</tr>
+
+		</form>
+		</table>
 
 
-
-
-?>
+</body>
+</html>
