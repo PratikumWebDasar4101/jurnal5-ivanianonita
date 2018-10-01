dcc20e3448442f7ab37761e1751e05c370b67b57
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-10-01 10:19:59 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,6 +1,23 @@
 <?php
+$nim=$_POST['nim'];
+$nama=$_POST['nama'];
+$email=$_POST['email'];
+$komentar=$_POST['komentar'];
+$hitung=count(explode(" ",$komentar));
 
+echo "Nim	: $nim <br>";
+echo "Nama : $nama<br>";
+echo "Email : $email";
 
+echo "<br>Jumlah Kata : $hitung";
+
+if($hitung <5){
+	echo"<br>Maaf Jumlah kata Kurang dari 5 kata ";
+}
+else
+{
+	die("<br><br>$komentar");
+}
 
 
 
